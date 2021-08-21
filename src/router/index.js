d6f4fe31ba9c8bcfb63acc8741f00f2c76304988
Rock Wu <948301454@qq.com>
2021-08-21 12:18:22 +0800

    feat: finish the app

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EventList from '../views/EventList.vue'
+import EventDetails from '../views/EventDetails.vue'
 import About from '../views/About.vue'
 
 const routes = [
@@ -15,6 +16,12 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: About,
+  },
+  {
+    path: '/event/:id',
+    name: 'EventDetails',
+    props: true, //该视图对应的组件的props中将可以通过设置id属性获取id的值
+    component: EventDetails,
   },
 ]
 
